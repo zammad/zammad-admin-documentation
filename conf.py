82b15163a12a2438f5b00622895e3a1c43e2005d
Johannes Nickel <jn@znuny.com>
2017-04-21 09:43:27 +0200

    updated config and added images

--- conf.py
+++ conf.py
@@ -1,14 +1,18 @@
-locale_dirs = ['locale/']
-gettext_compact = False
-
-source_suffix = '.rst'
-
-master_doc = 'index'
-exclude_patterns = ['_build', 'html', 'doctrees']
-language = "en"
-
 import sphinx_rtd_theme
 
-html_theme = "sphinx_rtd_theme"
+html_logo = "images/zammad_logo_70x61.png"
+html_favicon = "images/favicon.ico
+project = u'Zammad'
+copyright = u'2017, Zammad'
+author = u'Zammad'
 
+source_suffix = '.rst'
+master_doc = 'index'
+exclude_patterns = ['_build', 'html', 'doctrees']
+
+locale_dirs = ['locale/']
+gettext_compact = False
+language = "en"
+
+html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
