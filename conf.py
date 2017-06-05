eafb0cb1958031f103f7a391a133eb8ff367b898
Johannes Nickel <jn@znuny.com>
2017-06-05 18:03:37 +0200

    changed css path in config

--- conf.py
+++ conf.py
@@ -39,6 +39,6 @@ else:
         'css_files': [
             'https://media.readthedocs.org/css/sphinx_rtd_theme.css',
             'https://media.readthedocs.org/css/readthedocs-doc-embed.css',
-            '_static/theme_overrides.css',
+            '_static/theme/theme_overrides.css'
         ],
     }
