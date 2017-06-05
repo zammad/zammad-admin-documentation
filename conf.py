3a8f79522712741005ac3958c40358b405d25ba4
Johannes Nickel <jn@znuny.com>
2017-06-05 17:58:52 +0200

    Added custom CSS switch to conf file

--- conf.py
+++ conf.py
@@ -1,3 +1,6 @@
+import sys
+import os
+
 import sphinx_rtd_theme
 
 html_logo = "images/zammad_logo_70x61.png"
@@ -13,6 +16,8 @@ exclude_patterns = ['_build', 'html', 'doctrees']
 locale_dirs = ['locale/']
 gettext_compact = False
 language = "en"
+
+html_static_path = ['_static']
 
 html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
