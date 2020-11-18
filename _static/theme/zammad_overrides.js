09a7bab3039a4f1f937dc661c77b2ee520669adb
Marcel Herrguth <github@thehomeofanime.de>
2020-11-18 22:19:43 +0100

    Maintenance: Improve javascript and CSS
    On smaller screens the button could overlay parts of the content. Also, a shadow on the bottom now makes it clear that there's "more to come".

--- _static/theme/zammad_overrides.js
+++ _static/theme/zammad_overrides.js
@@ -18,23 +18,59 @@ function shortPre(i) {
    }
 }
 
+// Update <pre> elements
 function WorkPres(item, index) {
-   if( item.clientHeight > 250 ){
-      var button = document.createElement('button');
-      button.setAttribute('onclick', 'shortPre(' + index + ')');
-      button.setAttribute('id', 'button-pre-' + index);
-      $(button).html('Expand to full size ...').addClass('shorten-pre-button');
+   var size = 250;
 
+   if ( !document.getElementById(item) ){
       item.setAttribute('id', 'pre-' + index);
-      item.appendChild(button);
+   }
+
+   if( item.clientHeight > size ){
+      
+      if ( !document.getElementById('button-' + item) ){
+         var button = document.createElement('button');
+         button.setAttribute('onclick', 'shortPre(' + index + ')');
+         button.setAttribute('id', 'button-pre-' + index);
+         $(button).html('Expand to full size ...').addClass('shorten-pre-button');      
+         item.appendChild(button);
+
+         console.log("added button for big <pre> (pre-" + index + ")")
+      }
+      
       item.classList.add('shorten-pre');
    }
 };
 
-// onLoad run through all pres and add shorten classes to those that are quite long
-window.addEventListener('load', (event) => {
+// Provide all tabs with onClick action
+function WorkTabs(item) {
+   item.setAttribute('onclick', 'CalculatePres()');
+}
+
+// Function to fire (re)-calculation for <pre> elements
+function CalculatePres(){
    pres = document.getElementsByTagName('pre');
-   for(let i = 0; i < pres.length; i++){ 
+   for(var i = 0; i < pres.length; i++){ 
       WorkPres(pres[i], i);
    };
-});
+   console.log("<pre> elements reviewed ...")
+}
+
+function AddOnClick(){
+   // Find all possible tabs to add onClick actions
+   console.log("Add onClick actions for <a>-data-tabs (if any) ...")
+   tabs = document.getElementsByTagName('a');
+   for(var i = 0; i < tabs.length; i++){ 
+      if ( tabs[i].hasAttribute('data-tab') ){
+         WorkTabs(tabs[i]);
+      }
+   };
+}
+
+function OnLoadRunner(){
+   CalculatePres();
+   AddOnClick();
+}
+
+// onLoad run through all pres and add shorten classes to those that are quite long
+window.addEventListener('load', OnLoadRunner, false);
