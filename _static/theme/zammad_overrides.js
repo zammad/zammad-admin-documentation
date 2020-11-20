95d710e982b302764121b20038c3ea7577b49adf
Ryan Lue <hello@ryanlue.com>
2020-11-20 17:27:35 +0800

    Refactor JS logic for collapsing long code blocks

--- _static/theme/zammad_overrides.js
+++ _static/theme/zammad_overrides.js
@@ -1,73 +1,30 @@
 /* Custom overrides */
-/* No oversized <pre>, please - Thanks to @TJemxx */
 
-function onLoadRunner(){
-  calculatePres();
-  addOnClick();
+// No oversized <pre>, please - Thanks to @TJemxx
+function collapseLongCodeBlocks() {
+  const maxHeight = 250;
+
+  document.querySelectorAll('pre').forEach((el, i) => {
+    if (el.clientHeight <= maxHeight) return;
+
+    el.classList.add('collapsing-code-block');
+    if (el.querySelector(`.collapsing-code-block__toggle`)) return;
+
+    const button = document.createElement('button');
+    button.classList.add('collapsing-code-block__toggle');
+    button.onclick = (event) => {
+      event.target.parentElement.classList.toggle('collapsing-code-block--open')
+    };
+    el.appendChild(button);
+  });
 }
 
-// Function to fire (re)-calculation for <pre> elements
-function calculatePres(){
-  const pres = document.getElementsByTagName('pre');
-  for(let i = 0; i < pres.length; i++){ 
-    workPres(pres[i], i);
-  };
-}
+window.addEventListener('load', () => {
+  collapseLongCodeBlocks();
 
-// Update <pre> elements
-function workPres(item, index) {
-  const size = 250;
-
-  if ( !document.getElementById(item) ){
-    item.setAttribute('id', 'pre-' + index);
-  }
-
-  if( item.clientHeight > size ){
-    
-    if ( !document.getElementById('button-' + item) ){
-      const button = document.createElement('button');
-      button.setAttribute('onclick', 'shortPre(' + index + ')');
-      button.setAttribute('id', 'button-pre-' + index);
-      $(button).html('Expand to full size ...').addClass('shorten-pre-button');    
-      item.appendChild(button);
-    }
-    
-    item.classList.add('shorten-pre');
-  }
-};
-
-function shortPre(i) {
-  const attr = 'pre-' + i;
-  const pre = document.getElementById(attr);
-  const button = document.getElementById('button-' + attr);
-
-  if ( pre.classList.contains('shorten-pre')) {
-    pre.classList.remove('shorten-pre');
-    pre.classList.add('release-shorten-pre');
-
-    $(button).html('Reduce to smaller size ...')
-  } else {
-    pre.classList.remove('release-shorten-pre');
-    pre.classList.add('shorten-pre');
-
-    $(button).html('Expand to full size ...')
-  }
-}
-
-function addOnClick(){
-  // Find all possible tabs to add onClick actions
-  const tabs = document.getElementsByTagName('a');
-  for(let i = 0; i < tabs.length; i++){ 
-    if ( tabs[i].hasAttribute('data-tab') ){
-      workTabs(tabs[i]);
-    }
-  };
-}
-
-// Provide all tabs with onClick action
-function workTabs(item) {
-  item.setAttribute('onclick', 'calculatePres()');
-}
-
-// onLoad run through all pres and add shorten classes to those that are quite long
-window.addEventListener('load', onLoadRunner, false);
+  // click any <a data-tab="..."></a> link
+  // to manually re-compute if any code blocks need to be collapsed
+  document.querySelectorAll('a').forEach((el) => {
+    if ('tab' in el.dataset) el.onclick = collapseLongCodeBlocks;
+  });
+}, false);
